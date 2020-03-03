29d802aa52e4ecf86d4c7c3475c4e64d326c2e58
igor_shayderov <i.shayderov@systtech.ru>
2020-03-03 10:59:10 +0200

    lesson 1 complete

--- 1_lesson/ideal_weight.rb
+++ 1_lesson/ideal_weight.rb
@@ -9,5 +9,5 @@ ideal_weight = (user_height.to_i - 110) * 1.15
 if (ideal_weight < 0) 
 	puts "Your height is already optimum"
 else 
-	puts "#{user_name.capitalize} your optimum weight is #{ideal_weight}."
+	puts "#{user_name.capitalize} your ideal weight is #{ideal_weight}."
 end
